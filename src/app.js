f3454c062f50812e6501b157c6992cb90ba8237f
Sanskar Agarwal <196518365+actualsanskar@users.noreply.github.com>
2025-06-02 19:00:50 +0530

    fix cors middleware

--- src/app.js
+++ src/app.js
@@ -5,14 +5,16 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 
-app.cors({
+app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-})
+}))
 
 app.use(express.json({limit: '16kb'}));
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true, limit: '16kb'}))
 app.use(cookieParser());
+
+
 
 export {app}
